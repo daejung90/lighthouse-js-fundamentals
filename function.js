452c4cf3777ada9515a1993416ed63b1116cba3c
daejung90 <djung.ca@gmail.com>
2022-09-24 14:02:32 -0400

    added is odd function

--- function.js
+++ function.js
@@ -17,11 +17,17 @@ const greeting = sayHello ('Fernanda')
 console.log(greeting)
 
 //isEven FUNCTION
-const isEven = function (num) {
-    return num % 2 === 0;
-}
-const tenIsEven = isEven(10);
-const fifteenisEven = isEven (15);
+// const isEven = function (num) {
+//     return num % 2 === 0;
+// }
+// const tenIsEven = isEven(10);
+// const fifteenisEven = isEven (15);
 
-console.log(tenIsEven)
-console.log(fifteenisEven)
+// console.log(tenIsEven)
+// console.log(fifteenisEven)
+
+const isOdd = function (num){
+    return num % 2 !== 0;
+}
+console.log('3 is odd: ' + isOdd(3));
+console.log('8 is odd: ' + isOdd(8))
