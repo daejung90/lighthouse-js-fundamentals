8e2bfdca902aa64df020a1214c5f1d4d5c3d633a
daejung90 <djung.ca@gmail.com>
2022-09-24 13:56:42 -0400

    other function practice

--- hello.js
+++ hello.js
@@ -1,10 +1,27 @@
+//Basic FUNCTIONS
 // const sayHello = function (){
 //     console.log('Hello, world!');
 // }
-
 // sayHello();
 
-const sayHello = function(name){
-    console.log('Hello ' + name)
+// const sayHello = function(name){
+//     console.log('Hello ' + name)
+// }
+// sayHello('Dae')
+
+//RETURN
+const sayHello = function (name){
+    return 'Hello, ' + name;
 }
-sayHello('Dae')
+const greeting = sayHello ('Fernanda')
+console.log(greeting)
+
+//isEven FUNCTION
+const isEven = function (num) {
+    return num % 2 === 0;
+}
+const tenIsEven = isEven(10);
+const fifteenisEven = isEven (15);
+
+console.log(tenIsEven)
+console.log(fifteenisEven)
