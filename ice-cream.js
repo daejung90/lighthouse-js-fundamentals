acbd79c8bd4df44cfff0c4d2a470c2228d011156
daejung90 <djung.ca@gmail.com>
2022-09-26 11:53:24 -0400

    ice cream arry challenge

---
+++ ice-cream.js
@@ -0,0 +1,20 @@
+// const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+
+// iceCreamFlavours.push = 'root beer';
+// iceCreamFlavours
+
+// console.log(iceCreamFlavours);
+// console.log(iceCreamFlavours[1]);
+// console.log(iceCreamFlavours.length)
+
+
+let iceCreamFlavours = ["chocolate", "vanilla", "cookes and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+
+console.log(iceCreamFlavours.length);
