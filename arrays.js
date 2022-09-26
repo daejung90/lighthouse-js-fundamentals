7da36bbb17d5a954543362ea2f3bf790636c24d4
daejung90 <djung.ca@gmail.com>
2022-09-26 11:34:02 -0400

    arryas operations and properties

---
+++ arrays.js
@@ -0,0 +1,12 @@
+const director = 'Steven Spielberg';
+const films = [ 'Jaws', 'Raiders of the Lost Ark', 'A.I'];
+
+films[2] = 'Jurassic Park';
+films;
+films.push('A.I');
+films;
+
+console.log(films)
+
+films.push('E.T');
+console.log(director + ' is the director of ' + films.length + ' films.')
