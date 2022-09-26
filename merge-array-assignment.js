528d35075ae58cd54dff4837e64590503322a663
daejung90 <djung.ca@gmail.com>
2022-09-26 14:52:28 -0400

    merge-assignment

---
+++ merge-array-assignment.js
@@ -0,0 +1,12 @@
+function merge (a, b){
+    const merged = a.concat(b);
+    return merged.sort(function (ar1, ar2) {
+        return ar1 > ar2;
+    });
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
+
+
